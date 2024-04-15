d3036806906eceffa97113b7b973e4d0bf363f1d
Phobia <filipe_msr@hotmail.com>
2024-04-15 19:22:10 -0300

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/be3c2785-bc2a-4808-a0a7-9871cdedfcc4-dt7dfn.jpg",
   "https://utfs.io/f/f95b76fa-e9d4-4eb0-a07b-1101bf3fadfe-rxgda6.jpg",
@@ -10,12 +12,18 @@ const mockImages = mockUrls.map((url, index) => ({
   url
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany()
+  console.log(posts)
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages].map((image, i) => (
+          <div key={`${image.id}`} className="w-48">
             <img src={image.url} className="w-full" />
           </div>
         ))}
